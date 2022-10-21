b934908b01cd64ef723a2231d3667b8e1265a11a
Rohini KR <58960190+Rohini-KR@users.noreply.github.com>
2022-10-21 16:33:06 +0100

    Update streamlit_app.py
    changes made to show only fruits selected are displayed

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Add picklist here to allow user to choose fruits included in their smoothie
-streamlit.multiselect("Pick some fruits : ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits : ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_show = my_fruit_list.loc(fruits_selected)
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits.show)
