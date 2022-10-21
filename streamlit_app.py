fbf9359ce23884273414bfac0974b85597aa3546
Rohini KR <58960190+Rohini-KR@users.noreply.github.com>
2022-10-21 16:36:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Add picklist here to allow user to choose fruits included in their smoothie
 fruits_selected = streamlit.multiselect("Pick some fruits : ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-fruits_show = my_fruit_list.loc(fruits_selected)
+fruits_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_show)
