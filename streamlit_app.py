abd1d30a4d5b5b5876b816066fe66a20bbe95483
Rohini KR <58960190+Rohini-KR@users.noreply.github.com>
2022-10-21 16:12:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header(' Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍵Kyle Spinach & Rocket Smoothie')
 streamlit.text('🍳Hard-Boiled & Free-Range Egg')
+
+streamlit.header('🍎🍌Buid your own smoothie 🍑🥝')
